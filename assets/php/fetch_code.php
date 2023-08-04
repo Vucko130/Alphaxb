5895de3efe8e655e8fb8465e04c0cdd8ffc71f1a
Jalish Mahmud Sujon <92352731+Vucko130@users.noreply.github.com>
2023-08-04 23:15:39 +0600

    trying to fix source hound

--- assets/php/fetch_code.php
+++ assets/php/fetch_code.php
@@ -6,21 +6,17 @@ $url = $_GET['url'];
 $code = file_get_contents($url);
 
 // Determine the content type based on the file extension
-$contentType = 'text/plain';
 $extension = pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_EXTENSION);
 if ($extension === 'html') {
-    $contentType = 'text/html';
+    header('Content-Type: text/html');
 } elseif ($extension === 'css') {
-    $contentType = 'text/css';
+    header('Content-Type: text/css');
 } elseif ($extension === 'js') {
-    $contentType = 'application/javascript';
+    header('Content-Type: application/javascript');
 } elseif ($extension === 'php') {
-    $contentType = 'text/plain'; // Set the content type to plain text for PHP code
+    header('Content-Type: text/plain'); // Set the content type to plain text for PHP code
 }
 
-// Set the response headers
-header('Content-Type: ' . $contentType);
-
-// Return the code as JSON
-echo json_encode(array('code' => $code));
+// Return the code
+echo $code;
 ?>
